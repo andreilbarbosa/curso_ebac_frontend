d43a18da5de887fc9ef126989d5638c20c0436bc
andrei <andreilimabarbosa@outlook.com>
2025-08-26 18:08:18 -0300

    feat(imagemin): Adiciona a compressão de imagens ao projeto, como também as imagens e folders.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
 
 
 function compilaSass(){
@@ -11,6 +12,12 @@ function compilaSass(){
     .pipe(gulp.dest('./build/styles'))
 }
 
+function compilaImages(){
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'))
+}
 
 
-exports.default = compilaSass;
+
+exports.default = compilaImages;
